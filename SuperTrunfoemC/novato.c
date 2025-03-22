4f95b6ae59d75888eda89d32795c5706dd61e1c6
Évelim Dornelles <dorrrnelles@gmail.com>
2025-03-22 08:24:04 -0300

    upload

--- SuperTrunfoemC/novato.c
+++ SuperTrunfoemC/novato.c
@@ -2,9 +2,7 @@
 
 int main()
 {
-       char estado1, estado2;
-       char codigo1[4], codigo2[4];
-       char nomeCidade1[50], nomeCidade2[50];
+
        int populacao1, populacao2;
        float area1, area2, pib1, pib2;
        int pontosTuristicos1, pontosTuristicos2;
