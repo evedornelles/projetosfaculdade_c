7d5b2e8f8f7d4c65e35f2b3074f1980e0d481c29
Évelim Dornelles <dorrrnelles@gmail.com>
2025-03-23 08:46:00 -0300

    upload

--- SuperTrunfoemC/novato.c
+++ SuperTrunfoemC/novato.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+       char estado1, estado2;
+       char codigo1[4], codigo2[4];
+       char nomeCidade1[50], nomeCidade2[50];
+       int populacao1, populacao2;
+       float area1, area2, pib1, pib2;
+       int pontosTuristicos1, pontosTuristicos2;
 
        printf("Digite os dados da primeira carta:\n");
        printf("Estado (A-H): ");
