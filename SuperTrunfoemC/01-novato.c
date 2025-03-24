57f341d4abee0a6437cfd3bb4136d55f989d5874
Évelim Dornelles <166746018+evedornelles@users.noreply.github.com>
2025-03-24 20:21:31 -0300

    Update 01-novato.c

--- SuperTrunfoemC/01-novato.c
+++ SuperTrunfoemC/01-novato.c
@@ -1,53 +1,94 @@
 #include <stdio.h>
 
+typedef struct
+{
+       char estado[3];
+       char codigo[10];
+       char nome[50];
+       int populacao;
+       float area;
+       float pib;
+       int pontos_turisticos;
+       float densidade_populacional;
+       float pib_per_capita;
+} Carta;
+
+double calcular_densidade_populacional(int populacao, float area)
+{
+       return populacao / area;
+}
+
+double calcular_pib_per_capita(float pib, int populacao)
+{
+       return pib / populacao;
+}
+
+void comparar_cartas(Carta carta1, Carta carta2, char atributo[])
+{
+       printf("Comparação de cartas (Atributo: %s):\n", atributo);
+       float valor1, valor2;
+
+       if (strcmp(atributo, "Populacao") == 0)
+       {
+              valor1 = carta1.populacao;
+              valor2 = carta2.populacao;
+       }
+       else if (strcmp(atributo, "Area") == 0)
+       {
+              valor1 = carta1.area;
+              valor2 = carta2.area;
+       }
+       else if (strcmp(atributo, "PIB") == 0)
+       {
+              valor1 = carta1.pib;
+              valor2 = carta2.pib;
+       }
+       else if (strcmp(atributo, "Densidade Populacional") == 0)
+       {
+              valor1 = carta1.densidade_populacional;
+              valor2 = carta2.densidade_populacional;
+       }
+       else if (strcmp(atributo, "PIB per capita") == 0)
+       {
+              valor1 = carta1.pib_per_capita;
+              valor2 = carta2.pib_per_capita;
+       }
+       else
+       {
+              printf("Atributo inválido!\n");
+              return;
+       }
+
+       printf("Carta 1 - %s (%s): %.2f\n", carta1.nome, carta1.estado, valor1);
+       printf("Carta 2 - %s (%s): %.2f\n", carta2.nome, carta2.estado, valor2);
+
+       if ((strcmp(atributo, "Densidade Populacional") == 0 && valor1 < valor2) ||
+           (strcmp(atributo, "Densidade Populacional") != 0 && valor1 > valor2))
+       {
+              printf("Resultado: Carta 1 (%s) venceu!\n", carta1.nome);
+       }
+       else if (valor1 == valor2)
+       {
+              printf("Resultado: Empate!\n");
+       }
+       else
+       {
+              printf("Resultado: Carta 2 (%s) venceu!\n", carta2.nome);
+       }
+}
+
 int main()
 {
-       char estado1, estado2;
-       char codigo1[4], codigo2[4];
-       char nomeCidade1[50], nomeCidade2[50];
-       int populacao1, populacao2;
-       float area1, area2, pib1, pib2;
-       int pontosTuristicos1, pontosTuristicos2;
+       Carta carta1 = {"SP", "C01", "São Paulo", 12300000, 1521.11, 700000000000, 15};
+       Carta carta2 = {"RJ", "C02", "Rio de Janeiro", 6000000, 1182.30, 360000000000, 10};
 
-       printf("Digite os dados da primeira carta:\n");
-       printf("Estado (A-H): ");
-       scanf(" %c", &estado1);
-       printf("Código: ");
-       scanf(" %s", codigo1);
-       printf("Nome da cidade: ");
-       scanf(" %49s", nomeCidade1);
-       printf("População: ");
-       scanf(" %d", &populacao1);
-       printf("Área (km²): ");
-       scanf(" %f", &area1);
-       printf("PIB: ");
-       scanf(" %f", &pib1);
-       printf("Pontos turísticos: ");
-       scanf(" %d", &pontosTuristicos1);
+       carta1.densidade_populacional = calcular_densidade_populacional(carta1.populacao, carta1.area);
+       carta1.pib_per_capita = calcular_pib_per_capita(carta1.pib, carta1.populacao);
 
-       printf("\nDigite os dados da segunda carta:\n");
-       printf("Estado (A-H): ");
-       scanf(" %c", &estado2);
-       printf("Código: ");
-       scanf(" %s", codigo2);
-       printf("Nome da cidade: ");
-       scanf(" %49s", nomeCidade2);
-       printf("População: ");
-       scanf(" %d", &populacao2);
-       printf("Área (km²): ");
-       scanf(" %f", &area2);
-       printf("PIB: ");
-       scanf(" %f", &pib2);
-       printf("Pontos turísticos: ");
-       scanf(" %d", &pontosTuristicos2);
+       carta2.densidade_populacional = calcular_densidade_populacional(carta2.populacao, carta2.area);
+       carta2.pib_per_capita = calcular_pib_per_capita(carta2.pib, carta2.populacao);
 
-       printf("\nCarta 1:\n");
-       printf("Estado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n",
-              estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1);
-
-       printf("\nCarta 2:\n");
-       printf("Estado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos turísticos: %d\n",
-              estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2);
+       comparar_cartas(carta1, carta2, "Populacao");
 
        return 0;
 }
